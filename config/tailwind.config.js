503cd7c9f1a66be4c21f3c13d5d622f6590a27c5
tikaranimaru <tikaranimaru@gmail.com>
2022-05-09 15:41:54 +0900

    daisyuiをv2にアップデート

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('daisyui'),
+    require('daisyui/colors'),
+  ],
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
@@ -9,15 +14,21 @@ module.exports = {
   theme: {
     extend: {
       colors: require('daisyui/colors'),
+      themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-  ],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
